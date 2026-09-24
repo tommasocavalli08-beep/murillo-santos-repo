d386a810c71edde00b78011a0955c5438b491c77
tommasocavalli08-beep <tommasocavalli.08@gmail.com>
2026-09-24 09:02:21 +0200

    Add app/manifest.ts

---
+++ app/manifest.ts
@@ -0,0 +1,2 @@
+import type { MetadataRoute } from 'next';
+export default function manifest():MetadataRoute.Manifest{return{name:'Dr. Murillo Santos',short_name:'Dr. Murillo',description:'Ginecologia e Obstetrícia em Maringá',start_url:'/',display:'standalone',background_color:'#f5f4ef',theme_color:'#20313a',icons:[]}}
