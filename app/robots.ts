d199f8dbaa2d67ab73b54486371b9a5a30e9f0c8
tommasocavalli08-beep <tommasocavalli.08@gmail.com>
2026-09-24 09:02:17 +0200

    Add app/robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,2 @@
+import type { MetadataRoute } from 'next';
+export default function robots():MetadataRoute.Robots{const base=process.env.NEXT_PUBLIC_SITE_URL||'https://drmurillosantos.com.br';return{rules:[{userAgent:'*',allow:'/',disallow:['/editor-artigos','/api/']}],sitemap:`${base}/sitemap.xml`,host:base}}
