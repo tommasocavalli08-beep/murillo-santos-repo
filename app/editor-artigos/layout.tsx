7f53a5442b367ac24cce8c24fc1cb41cefd60238
tommasocavalli08-beep <tommasocavalli.08@gmail.com>
2026-09-24 09:02:22 +0200

    Add app/editor-artigos/layout.tsx

---
+++ app/editor-artigos/layout.tsx
@@ -0,0 +1,3 @@
+import type { Metadata } from 'next';
+export const metadata:Metadata={title:'Editor de artigos',robots:{index:false,follow:false}};
+export default function Layout({children}:{children:React.ReactNode}){return children}
